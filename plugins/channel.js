99c3b3d1f51c5e0617184712cc0b49dfad6ff577
black-spider436 <toxiclover416@gmail.com>
2025-08-03 19:05:42 +0300

    Create channel.js

---
+++ plugins/channel.js
@@ -0,0 +1,31 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const { ezra } = require("../fredi/ezra");
+
+ezra({ nomCom: "channel", reaction: "💐", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+    console.log("Commande saisie !!!s");
+    let z = 'Salut je m\'appelle JEEPERS CREEPER-XMD* \n\n ' + 'je suis un bot Whatsapp Multi-appareil voici la chaîne';
+    let d = ' developpé par *sir bravin*';
+    let varmess = z + d;
+    var lien = 'https://whatsapp.com/channel/0029VbAoW2dAInPcTs6Fmg1o';  // Remplacez cet URL par le lien que vous souhaitez envoyer
+    await zk.sendMessage(dest, { text: varmess + "\n" + lien });
+});
+
+console.log("mon test");
+
+;
+console.log("mon test");
+/*module.exports.commande = () => {
+  var nomCom = ["test","t"]
+  var reaction="☺️"
+  return { nomCom, execute,reaction }
+};
+
+async function  execute  (origineMessage,zok) {
+  console.log("Commande saisie !!!s")
+   let z ='Salut je m\'appelle *JEEPERS CREEPER-XMD* \n\n '+'je suis un bot Whatsapp Multi-appareil '
+      let d =' developpé par *sir bravin*'
+      let varmess=z+d
+      var img='https://whatsapp.com/channel/0029VbAoW2dAInPcTs6Fmg1o'
+await  zok.sendMessage(origineMessage,  { image:{url:img},caption:varmess});
+}  */ 
