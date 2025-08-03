b0d55877a0b1390baeb4d5877fb6535dc42059cd
black-spider436 <toxiclover416@gmail.com>
2025-08-03 17:07:49 +0300

    Create scn.js

---
+++ plugins/scn.js
@@ -0,0 +1,72 @@
+const {
+  ezra
+} = require(__dirname + "/../fredi/ezra");
+const {
+  format,
+  styletext
+} = require(__dirname + "/../fredi/mesfonctions");
+const moment = require("moment-timezone");
+const s = require(__dirname + "/../set");
+ezra({
+  'nomCom': "deploy",
+  'categorie': "deploy"
+}, async (_0x1b122e, _0x28446a, _0x43827c) => {
+  let {
+    ms: _0xba9598,
+    repondre: _0xa253d5
+  } = _0x43827c;
+  let {
+    cm: _0x3eefe7
+  } = require(__dirname + "/../fredi//ezra");
+  var _0x119fe5 = {};
+  var _0x4309cb = 'public';
+  if (s.MODE_PUBLIC != "oui") {
+    _0x4309cb = 'private';
+  }
+  var _0x43801a = {
+    'Général': '',
+    'Logo': '',
+    'hentai': '',
+    'weeb': '',
+    'Recherche': '',
+    'conversion': '',
+    'groupe': '',
+    'Téléchargement': ''
+  };
+  _0x3eefe7.map(async (_0xc32296, _0x3daccf) => {
+    if (!_0x119fe5[_0xc32296.categorie]) {
+      _0x119fe5[_0xc32296.categorie] = [];
+    }
+    _0x119fe5[_0xc32296.categorie].push(_0xc32296.nomCom);
+  });
+  const _0x455992 = moment(moment()).format("HH:MM:SS");
+  moment.tz.setDefault("asia/karachi ").locale('id');
+  const _0x1e5e1a = moment.tz("asia/karachi").format("DD/MM/YYYY");
+  console.log("date" + _0x1e5e1a);
+  console.log("temps " + _0x455992);
+  let _0x2d2a24 = " *𝙳𝙴𝙿𝙻𝙾𝚈 𝚈𝙾𝚄𝚁 𝙾𝚆𝙽 JEEPERS CREEPER-XMD *\n\n";
+  _0x2d2a24 += "\n𝙷𝙴𝙻𝙻𝙾𝚆 : " + _0xba9598.pushName + "\n*𝙳𝙴𝙿𝙻𝙾𝚈 𝚈𝙾𝚄𝚁 𝙾𝚆𝙽 JEEPERS CREEPER-XMD🤦 𝙷𝙴𝚁𝙴*\n𝚈𝙾𝚄𝚁 𝙳𝙴𝙿𝙻𝙾𝚈𝙴𝚁 : " + s.OWNER_NAME + "\n𝚈𝙾𝚄𝚁 𝙽𝙰𝙼𝙴 : " + _0xba9598.pushName + "\n*𝚃𝙾𝚃𝙰𝙻 JEEPERS CREEPER-XMD 𝚄𝚂𝙴𝚁𝚂 : 𝟷𝟶𝟾𝟿+𝚄𝚜𝚎𝚛𝚜*\n𝙱𝙾𝚃 𝙳𝙴𝙾𝙻𝙾𝚈𝙴𝙳 : 𝟷𝟹𝟿+ 𝚊𝚙𝚙𝚜 𝚍𝚎𝚙𝚕𝚘𝚢𝚎𝚍 𝚝𝚘𝚍𝚊𝚢\n*𝚃𝙾𝙿 𝙾𝙽 𝚃𝙷𝙴 𝙻𝙸𝙽𝙺 𝚃𝙾 𝙶𝙴𝚃 𝚂𝙴𝚂𝚂𝙸𝙾𝙽*\n\nhttps://jeepers-creepers-xmd-session-link-1.onrender.com/\n\n\n*𝚂𝚃𝙴𝙿𝚂 𝚃𝙾 𝙶𝙴𝚃 𝚂𝙴𝚂𝚂𝙸𝙾𝙽 𝙸𝙳*\n*𝟷.𝙾𝙿𝙴𝙽 𝙻𝙸𝙽𝙺 𝙰𝙱𝙾𝚅𝙴*\n*𝟸.𝙸𝙽𝚃𝙴𝚁 𝚈𝙾𝚄𝚁 𝚆𝙷𝙰𝚃𝚂𝙰𝙰𝙿 𝙽𝚄𝙼𝙱𝙴𝚁* *𝚆𝙸𝚃𝙷*\n*𝙲𝙾𝚄𝙽𝚃𝚈 𝙲𝙾𝙳𝙴 𝙴𝚐. 𝟸𝟻4717263𝚡𝚡𝚡*\n*𝟹.JEEPERS CREEPER-XMD 𝚆𝙸𝙻𝙻 𝚂𝙴𝙽𝙳 𝚈𝙾𝚄 𝙰 𝙲𝙾𝙳𝙴 𝙲𝙾𝙿𝚈*\n *𝚃𝙷𝙰𝚃 𝙲𝙾𝙳𝙴. 𝚃𝙷𝙴𝙽 𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿 𝚆𝙸𝙻𝙻* *𝚂𝙴𝙽𝚃*\n *𝟐𝟓47172636𝐱𝐱𝐱.*\n*𝟺.𝚃𝙾𝙿 𝙾𝙽 𝚃𝙷𝙰𝚃 𝐍𝐎𝐓𝐈𝐅𝐈𝐂𝐀𝐓𝐈𝐎𝐍* *JEEPERS CREEPER-XMD 𝙸𝙽𝚃𝙴𝚁* \n  *𝚃𝙷𝙴 𝙲𝙾𝙳𝙴 𝚃𝙷𝙰𝚃-JEEPERS CREEPER-XMD 𝚂𝙴𝙽𝚃 𝚈𝙾𝚄*\n*𝟻.𝙸𝚃 𝚆𝙸𝙻𝙻 𝙻𝙾𝙰𝙳 𝙵𝙾𝚁 𝚂𝙾𝙼𝙴𝚃𝙸𝙼𝙴* *𝚃𝙷𝙴𝙽 JEEPERS CREEPER-XMD*\n  *𝚆𝙸𝙻𝙻 𝚂𝙴𝙽𝙳 𝚈𝙾𝚄 𝙰 𝚂𝙴𝚂𝚂𝙸𝙾𝙽 𝙸𝙳 𝙸𝙽* *𝚈𝙾𝚄𝚁*\n  *𝙸𝙽𝙱𝙾𝚇 𝙸𝙽 𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿 𝙰𝚃 𝚈𝙾𝚄𝚁 𝙾𝚆𝙽* *𝙽𝚄𝙼𝙱𝙴𝚁*\n  *𝙲𝙾𝙿𝚈 𝚃𝙷𝙴 𝚂𝙴𝚂𝚂𝙸𝙾𝙽 𝙸𝙳 𝙰𝙽𝙳 𝚂𝙴𝙽𝙳* *𝚃𝙾 𝚈𝙾ur 𝙳𝙴𝙿𝙻𝙾𝚈𝙴𝚁*\n   \n   𝙽𝚘𝚝𝚎 : 𝚋𝚘𝚝 𝚊𝚛𝚎𝚗'𝚝 𝚏𝚘𝚛 𝚏𝚛𝚎𝚎\n   \n   𝚔𝚎𝚎𝚙 𝚞𝚜𝚒𝚗𝚐 JEEPERS CREEPER-XMD \n\n";
+  for (const _0x115688 in _0x119fe5) {
+    if (!_0x43801a[_0x115688]) {
+      _0x43801a[_0x115688] = '';
+    }
+    _0x2d2a24 += '';
+    for (const _0x344838 of _0x119fe5[_0x115688]) {
+      _0x2d2a24 += '';
+    }
+  }
+  try {
+    _0x28446a.sendMessage(_0x1b122e, {
+      'image': {
+        'url': "https://files.catbox.moe/tq1dtv.jpg"
+      },
+      'caption': _0x2d2a24,
+      'footer': "©TKM INC"
+    }, {
+      'quoted': _0xba9598
+    });
+  } catch (_0x193aab) {
+    console.log("🥵🥵 Menu erreur " + _0x193aab);
+    _0xa253d5("🥵🥵 Menu erreur " + _0x193aab);
+  }
+});
